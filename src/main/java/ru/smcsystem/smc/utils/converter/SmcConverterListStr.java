9d3726f796015da11d6d0a67671a44a96aaf8ec3
pwipo <pwipo.seits@gmail.com>
2024-02-17 21:30:01 +0300

    add SmcConverterListStr

---
+++ src/main/java/ru/smcsystem/smc/utils/converter/SmcConverterListStr.java
@@ -0,0 +1,39 @@
+package ru.smcsystem.smc.utils.converter;
+
+import ru.smcsystem.api.dto.ObjectArray;
+import ru.smcsystem.api.dto.ObjectElement;
+import ru.smcsystem.api.dto.ObjectField;
+import ru.smcsystem.api.enumeration.ObjectType;
+import ru.smcsystem.smc.utils.ModuleUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SmcConverterListStr extends SmcConverter<List<String>> {
+
+    @Override
+    public List<String> to(ObjectField field, ObjectElement objectElement) throws Exception {
+        if (field.isSimple()) {
+            return new ArrayList<>(List.of(field.getValue().toString()));
+        } else if (ModuleUtils.isObjectElement(field)) {
+            ObjectElement objectElementValue = ModuleUtils.getObjectElement(field);
+            if (objectElementValue.isSimple())
+                return objectElementValue.getFields().stream().map(ObjectField::getValue).map(Object::toString).collect(Collectors.toList());
+        } else if (ModuleUtils.isObjectArray(field)) {
+            ObjectArray objectArray = ModuleUtils.getObjectArray(field);
+            if (objectArray != null && objectArray.isSimple()) {
+                List<String> result = new ArrayList<>(objectArray.size());
+                for (int i = 0; i < objectArray.size(); i++)
+                    result.add(objectArray.get(i).toString());
+                return result;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public ObjectField from(String name, List<String> v, ObjectElement objectElement) throws Exception {
+        return new ObjectField(name, new ObjectArray((List) v, ObjectType.STRING));
+    }
+}
